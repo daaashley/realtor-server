fddf9e555d17ceb2a0c93d5a1fe23511d20a7bb9
daaashleywch <davidtestwch@gmail.com>
2019-12-12 22:35:22 -0600

    first new ux server commit

---
+++ build/precache-manifest.b78c934e2359cf20b97f3a0aff552d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d116257079f3c97aa5b081e576f7248c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e63fe09b708e43fddc0",
+    "url": "/static/css/main.9db38350.chunk.css"
+  },
+  {
+    "revision": "cefb4c50eb95c7b5e540",
+    "url": "/static/js/2.4853cdd2.chunk.js"
+  },
+  {
+    "revision": "7e63fe09b708e43fddc0",
+    "url": "/static/js/main.42998220.chunk.js"
+  },
+  {
+    "revision": "e6b9c16d3a1d2d1e0137",
+    "url": "/static/js/runtime~main.d348c7a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -1,19 +1,72 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const path = require("path");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/",()=>{
-    console.log('route was hit')
-})
+app.get("/", () => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
+app.post("/pdf", (req, res) => {
+  var name = req.body.name;
+  console.log("name", name);
+  // create a document and pipe to a blob
+  var doc = new PDFDocument();
 
+  // draw some text
+  doc.fontSize(25).text("Here is some vector graphics...", 100, 80);
+
+  // some vector graphics
+  doc
+    .save()
+    .moveTo(100, 150)
+    .lineTo(100, 250)
+    .lineTo(200, 250)
+    .fill("#FF3300");
+
+  doc.circle(280, 200, 50).fill("#6600FF");
+
+  // an SVG path
+  doc
+    .scale(0.6)
+    .translate(470, 130)
+    .path("M 250,75 L 323,301 131,161 369,161 177,301 z")
+    .fill("red", "even-odd")
+    .restore();
+
+  // and some justified text wrapped into columns
+  doc
+    .text("And here is some wrapped text...", 100, 300)
+    .font("Times-Roman", 13)
+    .moveDown()
+    .text("this is an example text", {
+      width: 412,
+      align: "justify",
+      indent: 30,
+      columns: 2,
+      height: 300,
+      ellipsis: true
+    });
+
+  // end and display the document in the iframe to the right
+  doc.pipe(fs.createWriteStream("public/file.pdf")); // write to PDF
+  doc.pipe(res); // HTTP response
+
+  // add stuff to PDF here using methods described below...
+
+  // finalize the PDF and end the stream
+  doc.end();
+});
 
 const port = process.env.PORT || 8080;
-const host = process.env.HOSTNAME || '0.0.0.0';
+const host = process.env.HOSTNAME || "0.0.0.0";
 
 // Launch Node.js server
 const server = app.listen(port, host, () => {
